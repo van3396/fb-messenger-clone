d5214f86ae14689d1dcddf7ca08b932f82196e1a
Qui <quivan3396@yahoo.com>
2020-12-29 17:22:14 -0500

    complete fb messenger clone

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
-import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import { FormControl, Input, InputLabel } from "@material-ui/core";
 import React, { useState, useEffect } from "react";
 import "../css-components/App.css";
 import db from "../firebase";
 import firebase from "firebase";
+import FlipMove from "react-flip-move";
+import SendIcon from "@material-ui/icons/Send";
+import { IconButton } from "@material-ui/core";
 
 import Message from "./Message";
 
@@ -19,7 +22,9 @@ function App() {
       .orderBy("timestamp", "desc")
       .onSnapshot((snapshot) => {
         // <-- this is  a listener inside of useEffect(which is also a listener)
-        setMessages(snapshot.docs.map((doc) => doc.data()));
+        setMessages(
+          snapshot.docs.map((doc) => ({ id: doc.id, message: doc.data() }))
+        );
       });
   }, []);
 
@@ -46,14 +51,31 @@ function App() {
   // console.log("set messages: ", messages);
   return (
     <div className="app">
+      <img src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100" />
       <h1> facebook messenger clone</h1>
-      <h2> Hello {userName}!</h2>
-      <form>
-        <FormControl>
-          <InputLabel>Enter a message...</InputLabel>
-          <Input value={input} onChange={(e) => setInput(e.target.value)} />
-          {/* <br /> */}
-          <Button
+      <h2> Welcome {userName}!</h2>
+
+      <form className="app_form">
+        <FormControl className="app_formControl">
+          {/* <InputLabel>Enter a message...</InputLabel> */}
+          <Input
+            className="app_input"
+            placeholder="Enter text here..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+
+          <IconButton
+            className="app_iconButton"
+            type="submit"
+            disabled={!input}
+            variant="contained"
+            color="primary"
+            onClick={sendMessage}
+          >
+            <SendIcon />
+          </IconButton>
+          {/* <Button
             type="submit"
             disabled={!input}
             variant="contained"
@@ -61,7 +83,7 @@ function App() {
             onClick={sendMessage}
           >
             send message
-          </Button>
+          </Button> */}
         </FormControl>
 
         {/* <input value={input} onChange={(e) => setInput(e.target.value)} /> */}
@@ -75,10 +97,11 @@ function App() {
           send message
         </Button> */}
       </form>
-
-      {messages.map((message) => (
-        <Message userName={userName} message={message} />
-      ))}
+      <FlipMove>
+        {messages.map(({ id, message }) => (
+          <Message key={id} userName={userName} message={message} />
+        ))}
+      </FlipMove>
     </div>
   );
 }
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,22 +1,22 @@
 import { Card, CardContent, Typography } from "@material-ui/core";
-import React from "react";
+import React, { forwardRef } from "react";
 import "../css-components/Message.css";
 
-function Message({ userName, message }) {
+const Message = forwardRef(({ userName, message }, ref) => {
   const isUser = userName === message.userName;
 
   return (
-    <div className={`message_card ${isUser && "message_user"}`}>
+    <div ref={ref} className={`message_card ${isUser && "message_user"}`}>
       {/*if this is true, then "message_user"*/}
       <Card className={isUser ? "message_userCard" : "message_guestCard"}>
         <CardContent>
           <Typography color="white" variant="h5" component="h2">
-            {message.userName}: {message.message}
+            {!isUser && `${message.userName || "Unknown User"}: `} {message.message}
           </Typography>
         </CardContent>
       </Card>
     </div>
   );
-}
+});
 
 export default Message;
